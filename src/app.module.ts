ee18a40497fdf24fda68457211e2ea0f644401df
nhan.vh <hoangnhan1907@gmail.com>
2020-08-29 11:34:33 +0700

    - Add RoleGurad

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { HttpModule, Module } from '@nestjs/common';
 
-import { AppService } from './app.service';
 import { GhtkModule } from './ghtk/ghtk.module';
 import { ProviderModule } from './provider/provider.module';
+import {APP_GUARD} from "@nestjs/core";
+import {RolesGuard} from "./common/guards";
 
 @Module({
     imports: [GhtkModule, ProviderModule, HttpModule],
     controllers: [],
-    providers: [AppService],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: RolesGuard,
+        },
+    ],
 })
 export class AppModule {}
--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,11 +1,16 @@
-// import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
-// import { Observable } from 'rxjs';
-//
-// @Injectable()
-// export class RolesGuard implements CanActivate {
-//     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-//         const param = context.switchToHttp().getRequest();
-//
-//         return true;
-//     }
-// }
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const getRequest = context.switchToHttp().getRequest();
+        getRequest.body = {
+            userId: getRequest.headers.userId,
+            ...getRequest.query,
+            ...getRequest.params,
+            ...getRequest.body,
+        };
+        return true;
+    }
+}
